a5df025b0c649d21d70cf64169d8ac1cdece3c79
Bruce <bhorth@vermbp.local>
2020-01-18 16:03:32 -0500

    basic calculations

--- temodar.py
+++ temodar.py
@@ -1,10 +1,26 @@
 import math
 
-print("hello world")
-x = 5
-print (x)
+def BSA(bWeight, bHeight):
+    return math.exp(
+         0.425*math.log(float(bWeight)
+        +0.725*math.log(float(bHeight)))
+        +math.log(71.84))/10000
 
-print(pow(x,2))
+def DailyDose(bWeight, bHeight, fDose):
+    BSA_value=BSA(bWeight,bHeight)
+    if (fDose in set(["y","Y"])):
+        return 150*BSA_value
+    else:
+        return 200*BSA_value
 
-print("hello again")
+def Main():
+    body_weight=input("Input body weight (kg): ")
+    body_height=input("Input body height (cm): ")
+    first_dose=input("Is this the first dose (Y/N): ")
 
+    print("")
+    print("weight=", body_weight, "kg; height=", body_height, "cm\n\n")
+    print("BSA=",BSA(body_weight,body_height))
+    print("Daily Dose=",DailyDose(body_weight, body_height,first_dose))
+
+Main()
