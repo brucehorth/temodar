bc14be61f5c826c0f095d27ca4d1c6ee6a1b41f1
Bruce <bhorth@vermbp.local>
2020-01-21 23:05:28 -0500

    print function

--- temodar_ood.py
+++ temodar_ood.py
@@ -7,7 +7,18 @@ class Patient:
         self.height=h
     
     @property
+    def BSA(self):
+        return math.exp(
+         0.425*math.log(float(self.weight))
+        +0.725*math.log(float(self.height))
+        +math.log(71.84))/10000
     
+    def Print(self):
+        print("Patient name: <",self.name,">, weight=",self.weight,"kg, height=",self.height,"cm")
+
+    def TemodarDose(self,firstDose):
+        
+
 
 def Main():
     print("My name is _______________ and my student number is _____________")
@@ -16,6 +27,9 @@ def Main():
     body_height=input("Input body height (cm): ")
     first_dose=input("Is this the first dose (Y/N): ")
 
-    print("")
+    p=Patient("Alice",body_weight,body_height)
+    p.Print()
+    print(".")
+    
  
 Main()
