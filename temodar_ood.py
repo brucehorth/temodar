bfc855922cd4991c9c61949693174fce2c7511c8
Bruce <bhorth@vermbp.local>
2020-01-21 22:49:42 -0500

    creating object-based version of calculator

---
+++ temodar_ood.py
@@ -0,0 +1,21 @@
+import math
+
+class Patient:
+    def __init__(self, n="anonymous", w=0, h=0):
+        self.name=n
+        self.weight=w
+        self.height=h
+    
+    @property
+    
+
+def Main():
+    print("My name is _______________ and my student number is _____________")
+
+    body_weight=input("Input body weight (kg): ")
+    body_height=input("Input body height (cm): ")
+    first_dose=input("Is this the first dose (Y/N): ")
+
+    print("")
+ 
+Main()
